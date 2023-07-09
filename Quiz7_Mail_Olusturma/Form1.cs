1f61104dc4d5fb41979af6e9be525f72e5374de3
f4t1hsalih <f4t1hsalih@gmail.com>
2023-07-09 08:57:29 +0300

    Add project files.

---
+++ Quiz7_Mail_Olusturma/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Quiz7_Mail_Olusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mail mail = new Mail();
+            mail.Ad = textBox1.Text;
+            mail.Soyad = textBox2.Text;
+            mail.Adres = textBox3.Text;
+
+            mail.Ekle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Mail mail = new Mail(textBox4.Text);
+            MessageBox.Show(mail.PinGoster());
+        }
+    }
+}
---
+++ Quiz7_Mail_Olusturma/Mail.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Quiz7_Mail_Olusturma
+{
+    class Mail
+    {
+        public Mail()
+        {
+        }
+
+        public Mail(string adres)
+        {
+            this.adres = adres;
+        }
+
+        private string ad;
+        public string Ad
+        {
+            get
+            {
+                return ad;
+            }
+
+            set
+            {
+                ad = value;
+            }
+        }
+
+        private string soyad;
+        public string Soyad
+        {
+            get
+            {
+                return soyad;
+            }
+
+            set
+            {
+                soyad = value;
+            }
+        }
+
+        private string adres;
+        public string Adres
+        {
+            get
+            {
+                return adres;
+            }
+
+            set
+            {
+                adres = value;
+            }
+        }
+
+        string SifreUret()
+        {
+            Random rnd = new Random();
+            int sayi = rnd.Next(1000, 10000);
+
+            return sayi.ToString();
+        }
+
+        public bool kontrol(string adres1)
+        {
+            StreamReader sr = new StreamReader(Application.StartupPath + "\\data.txt", Encoding.Default);
+            bool var = false;
+
+            while (!sr.EndOfStream)
+            {
+                string[] dizi = sr.ReadLine().Split(';');
+                if (dizi[2] == adres1) var = true;
+            }
+
+            return var;
+        }
+
+        public bool kontrol(string adres1, string sifre)
+        {
+            StreamReader sr = new StreamReader(Application.StartupPath + "\\data.txt", Encoding.Default);
+            bool var = false;
+
+            while (!sr.EndOfStream)
+            {
+                string[] dizi = sr.ReadLine().Split(';');
+                if ((dizi[2] == adres1) && (dizi[3] == sifre)) var = true;
+            }
+
+            return var;
+        }
+
+        public string PinGoster()
+        {
+            StreamReader sr = new StreamReader(Application.StartupPath + "\\data.txt", Encoding.Default);
+
+            string st = "";
+            while (!sr.EndOfStream)
+            {
+                string[] dizi = sr.ReadLine().Split(';');
+                if (dizi[2] == adres) st = dizi[3];
+            }
+
+            return st;
+        }
+
+        public void Ekle()
+        {
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\data.txt", true, Encoding.Default);
+
+            sw.WriteLine(ad + ";" + soyad + ";" + adres + ";" + SifreUret());
+            sw.Close();
+        }
+
+    }
+}
